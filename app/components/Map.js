0071154ee43abc833bc57b60005248d346f859e7
Tim Suchanek <tim.suchanek@gmail.com>
2015-09-26 22:24:41 +0200

    Added the map

--- app/components/Map.js
+++ app/components/Map.js
@@ -2,6 +2,12 @@
 
 import React from "react";
 
+var platform = new H.service.Platform({
+  'app_id': 'r7ElBPghk3pWDgiRQV8j',
+  'app_code': 'jdDUjr1MRWVtQKuvkg1NXw'
+});
+
+require('./map.styl');
 
 
 export default class Map extends React.Component {
@@ -14,10 +20,25 @@ export default class Map extends React.Component {
 	}
 
 
+	componentDidMount() {
+		// Obtain the default map types from the platform object:
+		var defaultLayers = platform.createDefaultLayers();
+
+		// Instantiate (and display) a map object:
+		var map = new H.Map(
+			React.findDOMNode(this.refs.map),
+			defaultLayers.normal.map,
+			{
+				zoom: 10,
+				center: { lat: 52.5, lng: 13.4 }
+			});
+	}
+
+
 
 	render() {
-		return <div>
-			<h1>Map</h1>
+		return <div className="map">
+			<div id="map" ref="map"></div>
 		</div>;
 	}
 }
