7bbadb7a0480ccfc28d3ee74a150a0325636575f
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2015-09-26 23:11:26 +0200

    some map stuff

--- app/components/Map.js
+++ app/components/Map.js
@@ -27,11 +27,36 @@ export default class Map extends React.Component {
 		// Instantiate (and display) a map object:
 		var map = new H.Map(
 			React.findDOMNode(this.refs.map),
-			defaultLayers.normal.map,
+			defaultLayers.normal.traffic,
 			{
 				zoom: 10,
 				center: { lat: 52.5, lng: 13.4 }
 			});
+
+        // Enable the event system on the map instance:
+        var mapEvents = new H.mapevents.MapEvents(map);
+
+        // Add event listener:
+        map.addEventListener('tap', function(evt) {
+            // Log 'tap' and 'mouse' events:
+            console.log(evt.type, evt.currentPointer.type); 
+        });
+
+        // Instantiate the default behavior, providing the mapEvents object: 
+        var behavior = new H.mapevents.Behavior(mapEvents);
+
+        // Create a group that can hold map objects:
+        group = new H.map.Group();
+
+        // Add the group to the map object (created earlier):
+        map.addObject(group);
+
+        // Create a marker:
+        marker = new H.map.Marker(map.getCenter());
+
+        // Add the marker to the group (which causes 
+        // it to be displayed on the map)
+        group.addObject(marker);
 	}
 
 
